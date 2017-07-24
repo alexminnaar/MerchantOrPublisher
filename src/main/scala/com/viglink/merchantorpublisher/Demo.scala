588b22c0e12b2339f26e59c8e9556bf2a540f9e4
Alex Minnaar <alexminnaar@ip-10-0-0-86.ec2.internal>
2017-07-24 11:47:36 -0700

    cleaned up demo and added some evaluation data in resources

--- src/main/scala/com/viglink/merchantorpublisher/Demo.scala
+++ src/main/scala/com/viglink/merchantorpublisher/Demo.scala
@@ -10,15 +10,18 @@ import scala.io.Source
 object Demo extends App {
 
   //get the absolute path of the training data folder in resources
-  val resourcesDirectory = new File("src/main/resources/TrainingData/")
-  val trainingDataPath = resourcesDirectory.getAbsolutePath
+  val trainingDataPath = new File("src/main/resources/trainingData/").getAbsolutePath
 
-  val trainTestSplit = Array(100.0,0.0)
+  //train model - not if test proportion is 0.0 then there will be no evaluation data shown
+  val trainTestSplit = Array(100.0, 0.0)
   val clf = Classifier.train(trainingDataPath, trainTestSplit)
 
-  val barneys_html = Source.fromFile("/users/alexminnaar/merchantOrPublisher_evaluation/barneys_1.html").getLines().toList.mkString(" ")
-  val avsforum_html = Source.fromFile("/users/alexminnaar/merchantOrPublisher_evaluation/avsforum_1.html").getLines().toList.mkString(" ")
+  //evaluate on a couple hold-out examples. Barney's is a merchant and avsforum is a publisher
+  val evaluationDataPath = new File("src/main/resources/evaluationData/").getAbsolutePath
+  val barneys_html = Source.fromFile(s"$evaluationDataPath/barneys_1.html").getLines().toList.mkString(" ")
+  val avsforum_html = Source.fromFile(s"$evaluationDataPath/avsforum_1.html").getLines().toList.mkString(" ")
 
+  //print classifier predictions
   val barneysPred = clf.classify(clf.getInstancePipe().instanceFrom(new Instance(barneys_html, "", "", "")))
   println("\n\nBARNEYS PAGE PREDICTION RESULTS:")
   println(barneysPred.getLabeling().getBestLabel().toString())
